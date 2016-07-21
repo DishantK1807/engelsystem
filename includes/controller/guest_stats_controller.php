a3b2e04368fe8b737f70a2b53b26730fbbb36cae
Dishant Khanna <dishant.khanna1807@gmail.com>
2016-07-21 15:17:52 +0530

    Revert "Model files"

--- includes/controller/guest_stats_controller.php
+++ includes/controller/guest_stats_controller.php
@@ -7,16 +7,16 @@ function guest_stats() {
     if($_REQUEST['api_key'] == $api_key) {
       $stats = array();
 
-      list($user_count) = user_count();
+      list($user_count) = sql_select("SELECT count(*) as `user_count` FROM `User`");
       $stats['user_count'] = $user_count['user_count'];
 
-      list($arrived_user_count) = user_count_arrived();
+      list($arrived_user_count) = sql_select("SELECT count(*) as `user_count` FROM `User` WHERE `Gekommen`=1");
       $stats['arrived_user_count'] = $arrived_user_count['user_count'];
 
-      $done_shifts_seconds = user_done_shifts();
+      $done_shifts_seconds = sql_select_single_cell("SELECT SUM(`Shifts`.`end` - `Shifts`.`start`) FROM `ShiftEntry` JOIN `Shifts` USING (`SID`) WHERE `Shifts`.`end` < UNIX_TIMESTAMP()");
       $stats['done_work_hours'] = round($done_shifts_seconds / (60*60), 0);
 
-      $users_in_action_source = user_action_source();
+      $users_in_action_source = sql_select("SELECT `Shifts`.`start`, `Shifts`.`end` FROM `ShiftEntry` JOIN `Shifts` ON `Shifts`.`SID`=`ShiftEntry`.`SID` WHERE UNIX_TIMESTAMP() BETWEEN `Shifts`.`start` AND `Shifts`.`end`");
       $stats['users_in_action'] = count($users_in_action_source);
 
       header("Content-Type: application/json");
